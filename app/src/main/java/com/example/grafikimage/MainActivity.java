ba104ead929fbbaf2b0bdf740acae9cc696fecda
diahsafitr25 <diahsafitri0125>
2019-11-01 16:11:39 +0700

    Project Selesai

---
+++ app/src/main/java/com/example/grafikimage/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.grafikimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.opengl.GLSurfaceView;
+
+
+public class MainActivity extends Activity {
+    private Button tekan;
+    private GLSurfaceView grafik1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tekan = (Button) findViewById(R.id.button1);
+        grafik1 = new HelloESSurfaceView(this);
+        setContentView(grafik1);
+    }
+    @Override
+    protected void onPause () {
+        super.onPause();
+        grafik1.onPause();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        grafik1.onResume();
+    }
+    class HelloESSurfaceView extends GLSurfaceView {
+        public HelloESSurfaceView(Context context) {
+            super(context);
+            setRenderer(new segitiga());
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
